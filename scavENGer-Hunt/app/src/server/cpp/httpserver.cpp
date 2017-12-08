fc095e8f0a8fac4a62b077329f1963b4cf94abad
Trent Chappus <tchappus@uwo.ca>
2017-12-08 15:05:57 -0500

    dev of GET requests for starting games, game info

--- scavENGer-Hunt/app/src/server/cpp/httpserver.cpp
+++ scavENGer-Hunt/app/src/server/cpp/httpserver.cpp
@@ -7,21 +7,34 @@
 #include <string>
 #include <locale>
 #include "SharedObject.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace Sync;
 class SharedBool {
 public:
     bool run;
+    ThreadSem currentgames_sem;
+    //vector of created threads
+    std::vector<std::thread*> runningThreads;
 };
 
+void ongoingGameThread(Socket conn) {
+
+}
 
 void threadProcedure(Socket conn) {
+    //get reference to shared memory
+    Shared<SharedBool> sharedMem("run");
 
     //read sent message from client
-    ByteArray recievedMsg;
-    conn.Read(recievedMsg);
+    ByteArray receivedMsg;
+    conn.Read(receivedMsg);
 
-    std::string msgString = recievedMsg.ToString();
+    std::string msgString = receivedMsg.ToString();
+
+    std::cout << "received: " << msgString << std::endl;
 
     std::string header;
 
@@ -44,11 +57,100 @@ void threadProcedure(Socket conn) {
             }
         }
     }
+    //GET requests
+    if (header == "GET") {
+        
+        //get game info
+        if (route.substr(0, 6) == "/game/") {
+            std::string gameID = route.substr(6, 6);
+            
+            //open file, use semaphore
+            sharedMem->currentgames_sem.Wait();
 
-    std::cout << "Request type: " << header << "  route: " << route << std::endl;
+            std::ifstream myfile ("currentgames.txt");
 
-    //close connection
-    conn.Close();
+
+            while (myfile.good()) {
+                std::string line;
+                getline(myfile, line);
+
+                std::istringstream lineSS(line);
+
+                std::vector<std::string> lineVector;
+
+                while ( lineSS ) {
+                    std::string value;
+
+                    if (!getline(lineSS, value, ',')) break;
+
+                    lineVector.push_back(value);
+                }
+
+                if (lineVector[0] == gameID) {
+                    std::cout << "your game was found: " << gameID << std::endl;
+
+                    std::stringstream response;
+                    response << "{ \"ID\": \"" << lineVector[0] << "\", \"Status\": \"" << lineVector[1] <<  "\", \"CurrentWord\": \"" << lineVector[2] << "\" }";
+                    
+                    ByteArray res (response.str());
+
+                    conn.Write(res);
+                    conn.Close();
+
+                    break;
+                }
+            }
+            myfile.close();
+
+            //signal semaphore
+            sharedMem->currentgames_sem.Signal();
+
+
+            //client wants to start a game
+        } else if (route.substr(0,11) == "/startgame/") {
+
+             //open file, use semaphore
+            sharedMem->currentgames_sem.Wait();
+
+            std::ifstream myfile ("currentgames.txt");
+
+            //check if there are any ongoing games
+            while (myfile.good()) {
+                std::string line;
+                getline(myfile, line);
+
+                std::istringstream lineSS(line);
+
+                std::vector<std::string> lineVector;
+
+                while ( lineSS ) {
+                    std::string value;
+
+                    if (!getline(lineSS, value, ',')) break;
+
+                    lineVector.push_back(value);
+                }
+
+                if (lineVector[1] == "WAITING") {
+
+                    //send game info
+                    std::stringstream response;
+                    response << "{ \"ID\": \"" << lineVector[0] << "\", \"Status\": \"" << lineVector[1] <<  "\", \"CurrentWord\": \"" << lineVector[2] << "\" }";
+                    
+                    ByteArray res (response.str());
+
+                    conn.Write(res);
+                    conn.Close();
+                    break;
+                }
+            }
+            myfile.close();
+
+            //signal semaphore
+            sharedMem->currentgames_sem.Signal();
+        
+        }
+    }
 }
 
 void serverThread() {
@@ -58,18 +160,15 @@ void serverThread() {
     //start server on port 2010
     SocketServer socketServer(2010);
 
-    //vector of created threads
-    std::vector<std::thread*> runningThreads;
-
     //accept connections and respond to them using a new thread
     while (sharedMem->run) {
         Socket conn = socketServer.Accept();
-        runningThreads.push_back(new std::thread(threadProcedure, conn));
+        sharedMem->runningThreads.push_back(new std::thread(threadProcedure, conn));
     }
 
     //finish up requests
-    for (int i = 0; i < runningThreads.size(); i++) {
-        (*runningThreads[i]).join();
+    for (int i = 0; i < sharedMem->runningThreads.size(); i++) {
+        (*sharedMem->runningThreads[i]).join();
     }
 }
 
@@ -79,7 +178,6 @@ int main(void)
 
     //get reference to shared memory
     Shared<SharedBool> sharedMem("run", true);
-
     sharedMem->run = true;
 
     //create main thread
