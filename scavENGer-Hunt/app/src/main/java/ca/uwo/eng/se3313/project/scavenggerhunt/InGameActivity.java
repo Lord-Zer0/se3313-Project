749ea43cd8512933b46c72caabe0a864be2a727b
Ashley <AshleyOttogalli@Ashleys-MacBook-Pro.local>
2017-12-07 20:35:45 -0500

    switching branch

---
+++ scavENGer-Hunt/app/src/main/java/ca/uwo/eng/se3313/project/scavenggerhunt/InGameActivity.java
@@ -0,0 +1,11 @@
+package ca.uwo.eng.se3313.project.scavenggerhunt;
+
+/**
+ * Created by AshleyOttogalli on 2017-12-07.
+ */
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+
+public class InGameActivity {
+
+
+}
--- scavENGer-Hunt/app/src/main/java/ca/uwo/eng/se3313/project/scavenggerhunt/MainActivity.java
+++ scavENGer-Hunt/app/src/main/java/ca/uwo/eng/se3313/project/scavenggerhunt/MainActivity.java
@@ -2,12 +2,37 @@ package ca.uwo.eng.se3313.project.scavenggerhunt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Initialize Visual Recognition client
+    VisualRecognition service=new VisualRecognition(
+            VisualRecognition.VERSION_DATE_2016_05_20,
+            getString(R.string.api_key)
+
+    );
+
+
+
+
+
+
+
+
+
 }
